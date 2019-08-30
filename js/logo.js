2dfc3a392e736e84e0cf4b23c0853ea8d1668815
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-08-30 14:48:36 +0200

    added header/nav/grid

--- js/logo.js
+++ js/logo.js
@@ -1,14 +1,1 @@
-const logo= document.querySelector('svg');
-const test=document.querySelector('.test');
 
-test.style.fill= 'black';
-
-logo.addEventListener('click', ()=>{
-
-if(test.style.fill === 'black'){
-  test.style.fill= 'yellow';
-}else if(test.style.fill === 'yellow'){
-  test.style.fill= 'black';
-}
-
-});
