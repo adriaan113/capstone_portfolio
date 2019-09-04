0432be3d4de179ffe25472bbedace185d9014c4a
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-09-04 16:37:42 +0200

    added lightbox and buttons on thumbnails

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 const thumbnail=document.querySelectorAll('.thumbnail');
 const border= document.querySelectorAll('.thumbnail svg');
 const borderPath = document.querySelectorAll('.thumbnail svg path');
+const btnContainer= document.querySelectorAll('.btn-container');
+const gridItem= document.querySelectorAll('.grid-item');
 
 
-
-for(let i=0;i<thumbnail.length;i++){
+for(let i=0;i<gridItem.length;i++){
 
   border[i].style.display= 'none';
 
-  thumbnail[i].addEventListener('mouseover',()=>{
+  gridItem[i].addEventListener('mouseover',()=>{
       border[i].style.display='block';
       const length= borderPath[i].getTotalLength();
       borderPath[i].style.strokeDasharray= length;
       borderPath[i].style.strokeDashoffset= length;
+
+      btnContainer[i].style.display='flex';
   });
 
-  thumbnail[i].addEventListener('mouseout',()=>{
+
+  gridItem[i].addEventListener('mouseout',()=>{
      border[i].style.display='none';
+     btnContainer[i].style.display= 'none';
 
       //borderPath.style.animationDirection='reverse';
   });
-
-
 }
