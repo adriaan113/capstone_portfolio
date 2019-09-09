179ee678cc2f7707ffc3e572f7b5544d09e04f45
Adriaan van der Ploeg <adriaanvanderploeg@gmail.com>
2019-09-09 17:12:08 +0200

    added link to top of page on logo

--- js/main.js
+++ js/main.js
@@ -107,3 +107,8 @@ logo.addEventListener('mouseover',()=>{
 logo.addEventListener('mouseout',()=>{
   path.style.stroke = '#fff';
 });
+
+logo.addEventListener('click',()=>{
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
